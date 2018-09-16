578499e23d5e606757e452bdc45dded6b0574e6b
Tanya Valdez <TValdez@digmap.com>
2018-09-15 22:23:18 -0700

    Initialize Firebase

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var x = 1;
-console.log(x);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCS381O2YES_uzCKYifPlCk-KlozT9emis",
+  authDomain: "bootcamptrainscheduler.firebaseapp.com",
+  databaseURL: "https://bootcamptrainscheduler.firebaseio.com",
+  projectId: "bootcamptrainscheduler",
+  storageBucket: "bootcamptrainscheduler.appspot.com",
+  messagingSenderId: "16170165991"
+};
+firebase.initializeApp(config);
